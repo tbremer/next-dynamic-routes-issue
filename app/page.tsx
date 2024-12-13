d44a5a8843ca0b6ed07bdc0c937fcb3ae2bbfa7d
Tom Bremer <tom@tbremer.com>
2024-12-13 15:09:53 -0600

    update index links

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { Link } from "./components/link";
 export default function Home() {
   return (
     <>
-      <Link href="/dynamic-route">dynamic route w/ promise function calls</Link>
+      <Link href="/dynamic-route">dynamic route w/ async function calls</Link>
       <br />
       <Link href="/dynamic-route-no-async-layout">
-        dynamic route w/o promise function calls
+        dynamic route w/o async function calls
       </Link>
     </>
   );
